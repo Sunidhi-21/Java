d87c4dc31a25af2e002458e1da0b10c9fff6f0a8
Sunidhi-21 <sunidhi.n.xworkz@gmail.com>
2020-10-28 20:39:21 +0530

    commit

---
+++ objectclassconcept/CloneTester.java
@@ -0,0 +1,13 @@
+package com.xworkz.objectclassconcept;
+
+public class CloneTester {
+
+	public static void main(String[] args) throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+		
+		CloneTester cloneTester = new CloneTester();
+			System.out.println(cloneTester.clone()!=cloneTester);
+		
+	}
+
+}
---
+++ objectclassconcept/GetClassTester.java
@@ -0,0 +1,20 @@
+package com.xworkz.objectclassconcept;
+
+public class GetClassTester {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		GetClassTester getClass = new GetClassTester();
+		System.out.println(getClass.getClass());
+		
+	}
+	
+//	@Override
+//	public final Class<Integer> getClass(){
+//		System.out.println(".....................");
+//	}
+
+	//Error: Cannot override the final method from Object
+	
+
+}
